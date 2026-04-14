8cc09680d6c3acb6b282305633a18ad6fb9885b9
k-ymmt <account@kymmt.net>
2026-04-14 19:10:52 +0900

    Initial structure: Cask formula and landing page

---
+++ Casks/abyssinian.rb
@@ -0,0 +1,21 @@
+cask "abyssinian" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://github.com/k-ymmt/Abyssinian-releases/releases/latest/download/Abyssinian-#{version}.zip"
+  name "Abyssinian"
+  desc "Cross-platform markdown note-taking app"
+  homepage "https://k-ymmt.github.io/Abyssinian-releases/"
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "AbyssinianMac.app"
+
+  zap trash: [
+    "~/Library/Application Support/AbyssinianMac",
+    "~/Library/Caches/app.kymmt.AbyssinianMac",
+    "~/Library/HTTPStorages/app.kymmt.AbyssinianMac",
+    "~/Library/Preferences/app.kymmt.AbyssinianMac.plist",
+  ]
+end
